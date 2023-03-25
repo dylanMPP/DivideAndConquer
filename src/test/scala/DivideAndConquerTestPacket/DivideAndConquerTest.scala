0df6c4945d96b43ece4128beda43d1cfcd10710a
Dylan Bermudez Cardona <dyez1110@gmail.com>
2023-03-25 15:35:58 -0500

    Tests implementation for 'numberOfInversions' and 'improvingQuickSort' done

--- src/test/scala/DivideAndConquerTestPacket/DivideAndConquerTest.scala
+++ src/test/scala/DivideAndConquerTestPacket/DivideAndConquerTest.scala
@@ -3,5 +3,26 @@ package DivideAndConquerTestPacket
 import DivideAndConquerPacket.DivideAndConquer
 
 class DivideAndConquerTest extends munit.FunSuite {
+  
+  def normalList = List(10,7,3,6,2,9)
+  def repeatedList = List(10,7,3,10,6,2,9)
+  def sortedList = List(1,2,3,10,22)
+
+
+  test("Number Of Inversions:") {
+    assert(DivideAndConquer.numberOfInversions(normalList)==4)
+    assert(DivideAndConquer.numberOfInversions(repeatedList)==5)
+    assert(DivideAndConquer.numberOfInversions(sortedList)==0)
+  }
+
+  test("Improved QuickSort:"){
+    assert(DivideAndConquer.improvingQuickSort(normalList) == List(2,3,6,7,9,10))
+    assert(DivideAndConquer.improvingQuickSort(repeatedList) == List(2,3,6,7,9,10,10))
+    assert(DivideAndConquer.improvingQuickSort(sortedList) == sortedList)
+  }
+
+  test("Closest Points:"){
+
+  }
 
 }
