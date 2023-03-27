a4efba14e383fe3f69fcb7edf510d2e3a34044d5
Dylan Bermudez Cardona <dyez1110@gmail.com>
2023-03-26 20:11:35 -0500

    Euclidean distance added

--- src/main/scala/DivideAndConquerPacket/DivideAndConquer.scala
+++ src/main/scala/DivideAndConquerPacket/DivideAndConquer.scala
@@ -1,17 +1,76 @@
 package DivideAndConquerPacket
 
+import scala.annotation.tailrec
+
 object DivideAndConquer extends App with IDivideAndConquer {
 
+  // NUMBER OF INVERSIONS -> Returns the number of inversions that are necessary to get the sorted list
   def numberOfInversions(list: List[Int]): Int =
     0
 
-
+  // IMPROVING QUICKSORT -> Returns the sorted list using quicksort, using a 3-way partition instead of a 2-way
   def improvingQuickSort(list: List[Int]): List[Int] =
     List()
 
-
+  // CLOSEST POINTS -> Returns the closest points of a list of points (the points are pair of int numbers)
   def closestPoints(list: List[List[Int]]): Double =
     0.0
 
+  // EUCLIDEAN DISTANCE -> Returns the distance between 2 points (pairs of int numbers) using the euclidean method
+  def euclideanDistance(firstPair: List[Int], secondPair: List[Int]): Double =
+    if (firstPair.length != 2 || secondPair.length != 2) {
+      -1.0
+    } else {
+      val firstPow = pow(difference(secondPair.head, firstPair.head), 2)
+      val secondPow = pow(difference(secondPair(1), firstPair(1)), 2)
 
+      val result = sum(firstPow, secondPow)
+      result
+    }
+
+  // PREDECESOR AND SUCESOR
+  def pred(n: Int): Int = if (n > 0) n - 1 else 0
+
+  def suc(n: Int): Int = n + 1
+
+  // SUM
+  def sum(number: Int, number2: Int): Int =
+    if (number == 0) number2 else suc(sum(pred(number), number2))
+
+  // DIFFERENCE
+  @tailrec
+  def difference(number: Int, number2: Int): Int =
+    if (number2 == 0) number else difference(pred(number), pred(number2))
+
+  // POW
+  def pow(base: Int, exp: Int): Int =
+    if (exp == 0) 1 else base * pow(base, pred(exp))
+
+
+  // MERGE SORT
+  def mergeSort(): Unit = {
+
+  }
+
+  // QUICKSORT PARTITION
+  /* def partition(list: List[Int], firstPosition: Int, lastPosition: Int, resultList: List[Int]): Unit =
+
+    val x = list(lastPosition)
+    val i = firstPosition - 1
+    val stop =  list.length-1
+
+    @tailrec
+    def recursiveFor(resultList: List[Int], position: Int): List[Int] ={
+      if(position == stop){
+        resultList
+      } else {
+        if (list(firstPosition) <= x)
+          recursiveFor(list, position + 1)
+        else
+          recursiveFor(list.updated(i, position), position + 1)
+      }
+    }
+
+    resultList = recursiveFor(list, firstPosition)
+    */
 }
--- src/test/scala/DivideAndConquerTestPacket/DivideAndConquerTest.scala
+++ src/test/scala/DivideAndConquerTestPacket/DivideAndConquerTest.scala
@@ -7,21 +7,26 @@ class DivideAndConquerTest extends munit.FunSuite {
   def normalList = List(10,7,3,6,2,9)
   def repeatedList = List(10,7,3,10,6,2,9)
   def sortedList = List(1,2,3,10,22)
+  def voidList = List()
 
 
   test("Number Of Inversions:") {
     assert(DivideAndConquer.numberOfInversions(normalList)==4)
     assert(DivideAndConquer.numberOfInversions(repeatedList)==5)
     assert(DivideAndConquer.numberOfInversions(sortedList)==0)
+    assert(DivideAndConquer.numberOfInversions(voidList)==0);
   }
 
   test("Improved QuickSort:"){
     assert(DivideAndConquer.improvingQuickSort(normalList) == List(2,3,6,7,9,10))
     assert(DivideAndConquer.improvingQuickSort(repeatedList) == List(2,3,6,7,9,10,10))
     assert(DivideAndConquer.improvingQuickSort(sortedList) == sortedList)
+    assert(DivideAndConquer.improvingQuickSort(voidList) == voidList)
   }
 
   test("Closest Points:"){
+
+
 
   }
 
