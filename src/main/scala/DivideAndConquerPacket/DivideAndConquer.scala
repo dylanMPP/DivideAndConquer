6c5c702f483bec9da384d02328347bc2dcd95dab
Dylan Bermudez Cardona <dyez1110@gmail.com>
2023-04-05 20:04:24 -0500

    MergeSort done. Number of inversions and Randomized Quicksort tried

--- src/main/scala/DivideAndConquerPacket/DivideAndConquer.scala
+++ src/main/scala/DivideAndConquerPacket/DivideAndConquer.scala
@@ -1,11 +1,30 @@
 package DivideAndConquerPacket
 
 import scala.annotation.tailrec
-
+import scala.util.Random
 object DivideAndConquer extends App with IDivideAndConquer {
 
   // NUMBER OF INVERSIONS -> Returns the number of inversions that are necessary to get the sorted list
-  def numberOfInversions(list: List[Int]): Int = ???
+  def numberOfInversions(list: List[Int]): Int =
+
+
+    if (alreadySorted(list, 0) == 0) {
+      0
+    } else {
+      if (list.length % 2 == 0) {
+        mergeSort(list, 0, 0)._2
+      } else {
+        // I subtract 1 to the result of the number of inversions given by 'modifiedMergeSort', because
+        // when the list length is odd, the center number is repeated in the two first partitions
+        val n = list.length / 2
+
+        if (list(n) > list(n + 1)) {
+          mergeSort(list, 0, 0)._2 - 1
+        } else {
+          mergeSort(list, 0, 0)._2
+        }
+      }
+    }
 
   // IMPROVING QUICKSORT -> Returns the sorted list using quicksort, using a 3-way partition instead of a 2-way
   def improvingQuickSort(list: List[Int]): List[Int] = ???
@@ -13,15 +32,147 @@ object DivideAndConquer extends App with IDivideAndConquer {
   // CLOSEST POINTS -> Returns the closest points of a list of points (the points are pair of int numbers)
   def closestPoints(list: List[List[Int]]): Double = ???
 
-  // MERGE SORT
-  def mergeSort(list: List[Int]): List[Int] = ???
+  // LIST ALREADY SORTED ?
+  // It returns 0 if the list is already sorted, 1 or more if isn't sorted
+  @tailrec
+  def alreadySorted(list: List[Int], cont: Int): Int =
+    list match
+      case Nil => cont
+      case head :: tail => tail match {
+        case Nil => cont
+        case head2 :: tail2 =>
+          if (head > head2) {
+            alreadySorted(tail, cont + 1)
+          } else {
+            alreadySorted(tail, cont)
+          }
+      }
 
-  def merge(l1: List[Int], l2: List[Int]) = ???
+  // MODIFIED MERGE SORT
+  def mergeSort(list: List[Int], inversions: Int, totalInv: Int): (List[Int], Int) =
+    // List(10,7,3,6,2,9)
+    val n = list.length / 2
+    // n = 3
+    // n = 1.5
+    // n = 0
+
+    // n = 1
+    // n = 0
+    // n = 0
+    if (n == 0) (list, inversions)
+    // 10, 0
+    // 7, 0
+    // 3, 0
+    else {
+      //l1: (10,7,3)
+      //l2: (6,2,9)
+
+      //l1: 10
+      //l2: (7,3)
+
+      //l1: 7
+      //l2: 3
+      val (l1, l2) = list splitAt n
+
+      // (3,7,10),
+      // (3,7), 1
+
+      /*totalInv = totalInv + tailMerge(mergeSort(l1, inversions, totalInv)._2 /* 7, 0*/,
+        mergeSort(l2, inversions, totalInv)._1 /* 3,0*/, inversions, List())._2*/
+
+      tailMerge(mergeSort(l1, inversions, totalInv)._1 /* 7, 0*/ ,
+        mergeSort(l2, inversions, totalInv)._1 /* 3,0*/ , inversions, List())
+      //                  (10,7,3), 0             |           (6,2,9), 0
+      //                  10, 0                   |           (7,3), 0
+      //                  7,0                     |           3,0
+
+    }
+
+  @tailrec
+  def tailMerge(l1: List[Int], l2: List[Int], inversions: Int, resultList: List[Int]): (List[Int], Int) =
+  // 7, 3
+
+  // 10, (3,7)
+  // 10, 7
+  // 10, Nil
+    (l1, l2) match {
+      case (Nil, _) => (resultList.reverse ++ l2, inversions)
+      //3::7::10::Nil
+      case (_, Nil) => (resultList.reverse ++ l1, inversions)
+      case (head1 :: tail1, head2 :: tail2) =>
+        if (head1 <= head2)
+          tailMerge(tail1, l2, inversions, head1 :: resultList)
+        else // (head1 > head2)
+        //        7, Nil, 1, 3::Nil
+
+        //      10, 7, 1, 3::Nil
+        //      10, Nil, 2, 7::3::Nil
+          tailMerge(l1, tail2, inversions + 1, head2 :: resultList)
+    }
+
+  /*def merge(l1: List[Int], l2: List[Int], inversions: Int): (List[Int], Int) =
+      l1 match
+        case Nil => (l2, inversions)
+        case head::tail => l2 match {
+          case Nil => (l1, inversions)
+          case head2::tail2 =>
+            if (head<head2) (head::merge(tail, l2, inversions)._1, merge(tail, l2, inversions)._2+1)
+            else (head2::merge(l1, tail2, inversions)._1, merge(l1, tail2, inversions)._2+1)
+        }*/
 
   // QUICKSORT
-  def quickSort(list: List[Int], firstPos: Int, lastPos: Int): List[Int] = ???
+  def random(start: Int, end: Int): Int = {
+    val random = new Random()
+    random.nextInt(end - start + 1) + start
+  }
 
-  def quickSortPartition(list: List[Int], firstPos: Int, lastPos: Int): Int = ???
+  def randomizedQuickSort(list: List[Int], firstPos: Int, lastPos: Int, resultList: List[Int]): List[Int] =
+    if(firstPos < lastPos) {
+      val q = randomizedPartition(list, firstPos, lastPos)._2
+
+      randomizedQuickSort(list, firstPos, q - 1, randomizedPartition(list, firstPos, lastPos)._1)
+      randomizedQuickSort(list, q + 1, lastPos, randomizedPartition(list, firstPos, lastPos)._1)
+    } else {
+      resultList
+    }
+
+  def randomizedPartition(list: List[Int], firstPos: Int, lastPos: Int): (List[Int],Int) =
+    val i = random(firstPos, lastPos)
+
+    val valueLastPos = list(lastPos - 1)
+    val valueI = list(i - 1)
+
+    val exchangedList = list.updated(lastPos - 1, valueI).updated(i - 1, valueLastPos)
+    partition(exchangedList, firstPos, lastPos)
+
+  // Devuelvo la lista que se va actualizando más la Q, la posición en la que se va a ir organizando
+  def partition(list: List[Int], firstPos: Int, lastPos: Int): (List[Int], Int) =
+    val x = list(lastPos - 1)
+    var i = firstPos - 1
+
+    @tailrec
+    def partitionFor(lastPos: Int, list: List[Int],
+                     cont: Int): (List[Int], Int) =
+
+      if (cont == (lastPos - 1)) {
+        (list, i)
+      } else if (list(cont) <= x) {
+        i = i + 1
+
+        val valueCont = list(cont)
+        val valueI = list(i)
+
+        partitionFor(lastPos, list.updated(i, valueCont).updated(cont, valueI), cont + 1)
+      } else {
+        partitionFor(lastPos, list, cont + 1)
+      }
+
+    val valueLastPos = list(lastPos - 1)
+    val valueI = list(i + 1)
+
+    (list.updated(lastPos - 1, valueI).updated(i + 1, valueLastPos), i + 1)
+
+
 
   // EUCLIDEAN DISTANCE -> Returns the distance between 2 points (pairs of int numbers) using the euclidean method
   def euclideanDistance(firstPair: List[Int], secondPair: List[Int]): Double =
